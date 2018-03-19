8972acf491375959f6c3ebb30e1e75198f568a33
cburns32 <christianburns1234@gmail.com>
2018-03-19 09:00:39 -0400

    Revised to pass values properly

--- Switch_Class.py
+++ Switch_Class.py
@@ -2,17 +2,17 @@ import simpy
 import random
 import logger
 
-<<<<<<< HEAD
 # TODO Implement logger
 # TODO Return connection status
 
 class Switch(object):
-    def __init__(self, env, name, msg, mac, ip, port):
+    def __init__(self, env, msg, name, mac, ip, port):
         self.env = env # Store a local reference to the simpy environment
+        self.name = name
         self.msg = msg # String of the message type
         self.mac = mac # Define general mac address variable
         self.ip = ip   # Define general ip address variable
-        self.port = port_assigned # Defines port for device to sit on (each port is an element in portList)
+        self.port = port # Defines port for device to sit on (each port is an element in portList)
         self.avail_ports = True # Used to avoid cache of more devices than ports available
         self.total_ports = 8    # 8 ports on switch
         # self.ports_disconnected = []
@@ -20,77 +20,39 @@ class Switch(object):
         self.portList = [] # Dontains each connected devices' information
         self.fifo_tracker = 0 # This keeps track of the order each device is
                               # stored in cache (should be same sequential order as portList appears)
-
+    #    if len(store_cache()) > 1:
+        self.store_cache()
+        print(msg, name)
 # store_cache function is used to record device name, mac, ip, port in a list of device definitions
-    def store_cache(msg, name, mac, ip, port_assigned):
-        if self.msg == 'Sending' and self.avail_ports == True:
-            self.fifo_tracer += 1
-            self.cache = {'name':self.name, 'mac':self.mac, 'ip':self.ip, 'Queue':self.fifo_tracker, 'port_assigned':self.port_assigned}
-            self.portList.append(cache)
+    def store_cache(self):
+        print "entered"
+        if self.msg == "Sending" and self.avail_ports == True:
+            self.cache = {'name':self.name, 'mac':self.mac, 'ip':self.ip, 'Queue':self.fifo_tracker, 'port_assigned':self.port}
+            self.portList.append(self.cache.copy())
+            self.fifo_tracker += 1
+            print "inside sending condition"
+            print str(self.portList)
+            print len(self.portList)
             self.msg = 'Stored'
-        if self.msg == 'Sending' and self.avail_ports == False:
+            return self.portList
+        if self.msg == "Sending" and self.avail_ports == False:
             self.msg = 'Error: All ports taken'
+            print msg
 
 # port_disconnected function is used to remove device from cache table
 # While msg == disconnect port, this searches for specific element with correct port_assigned,
 # then deletes it from portList
-    def port_disconnected(self, msg, port_assigned):
+    def port_disconnected(self):
         if self.msg == 'Disconnected port: ' + str(port_assigned):
             port_dc = port_assigned
-            for port_assigned in cache.keys():
-                if port_assigned == port_dc:
+            for self.port in cache.keys():
+                if self.port == port_dc:
                     del portList[fifo_tracer -1]
             self.msg = 'Removed from cache'
 
 # port_capacity prevents prevents too many entries from entering cache
-=======
-# implement logger
-# Next revision:
-#   - Comment accordingly
-#   - Decide on listing method used
-#     to store name, ip, mac, port, <interface>
-#   - Return connection status
-
-class Switch(object):
-    def __init__(self, env, msg, name, mac, ip, port):
-        self.env = env # Store a local reference to the simpy environment
-        self.msg = msg # String of the message type
-        self.mac = mac # Defines the object name where the data can be found
-        self.ip = ip
-        self.avail_ports = True
-        self.total_ports = 8
-        self.port_assigned = port
-        self.ports_disconnected = []
-        self.cache = []
-
-    def store_cache(msg, name, mac, ip, port_assigned):
-        if self.msg == 'Sending' and self.avail_ports == True:
-            self.cache[self.port_assigned].append(self.name, self.mac, self.ip, self.port_num)
-            print(self.cache[self.port_assigned])
-        '''
-            self.cache[self.port_assigned[0] = self.name
-            self.cache[self.port_assigned][1] = self.mac
-            self.cache[self.port_assigned][2] = self.ip
-            self.cache[self.port_assigned][3] = self.port_num
-        '''
-        if self.port_assigned in self.ports_disconnected:
-            self.ports_disconnected.remove(self.port_assigned)
-        self.msg = 'Stored'
-        if self.msg == 'Sending' and self.avail_ports == False:
-            self.msg = 'Error: All ports taken'
-
-    def port_disconnected(self, msg, port_assigned):
-        if self.msg == 'Disconnected port: ' + str(port_assigned):
-            self.cache[self.port_assigned][0] = None
-            self.cache[self.port_assigned][1] = None
-            self.cache[self.port_assigned][2] = None
-            self.cache[self.port_assigned][3] = None
-            self.ports_disconnected = [self.port_assigned]
-            self.msg = 'Removed from cache'
-
- # TODO Revise to Port_capacity function with designated port properly
->>>>>>> 1653507cbea213408e2efd703af6b2fa863fbca5
-    def port_capacity(self, msg, ports_occupied, avail_ports):
+    def port_capacity(self, avail_ports):
+        self.port_occupied = portList.index()
         if self.msg == 'Stored':
             self.ports_occupied += 1
         if self.msg =='Removed':
